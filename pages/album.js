a87f1736bbb26fa17ca8d4c8aa9f74d9dbb99499
dave-torr <djjabbua@gmail.com>
2020-11-19 16:54:25 -0500

    ver 1 done

--- pages/album.js
+++ pages/album.js
@@ -49,6 +49,46 @@ let song6={
     "songAudioFile": "/audioFiles/Chococolate.wav"
 }
 
+let musicCredits1=[
+    "Johan Vallejo", " -",
+    "Carlos Quilumba",  
+    "Alexei Chontasi", 
+    "Victor Hugo Rojas",
+     "Diego Bosmediano",
+     "Pablo Gutierrez",
+     "Sergio Espinosa",
+     "Nicolas Espinosa" 
+]
+let musicCredits2=[
+    "Saxofón alto, Dirección", "musical y composición", 
+    "Saxofón tenor",  
+    "Trombón", 
+    "Trombón",
+    "Trompeta", 
+    "Trompeta", 
+    "Bajo, composición", 
+    "Batería"
+]
+
+let musicCredits3=[
+    "Theo Montero",
+    "-",
+    "Daniel Gachet",
+    "Juan Pablo Rivas",
+    "Felipe Sanchez",
+    "Pablo Lalama &",
+    "Pichirilo Radioactivo"
+]
+let musicCredits4=[
+    "Grabación, Edición", 
+    "Producción",
+    "Mezcla",
+    "Master",
+    "Stage Manager",
+    "Dirección Creativa",
+    "-"
+]
+
 // JSON 
 export default function Album(){
 
@@ -98,28 +138,45 @@ export default function Album(){
         )
     }
 
-
-
-
-
     const songPlayer=(eachSong)=>{
-        let anIcons= iconArray.map((elem, i)=>
-            <div className={styles.eachIconCont}>
-                <Image
-                    src={`/${elem}`}
-                    alt={`${elem}`}
-                    width={40}
-                    height={40}
-                    quality={100}
-                />
-            </div>
-        )
 
         return(
             <>
                 <div className={styles.playerGenCont}> 
                     <div className={styles.socialMediaLinks}>
-                        {anIcons}
+                        <div className={styles.eachIconCont}>
+                            <a target="_blank" rel="noreferrer noopener" href="https://www.youtube.com/channel/UCdi62GlZckid7nfbb0Gp5Tw?view_as=subscriber" > 
+                                <Image
+                                    src="/youtube.png"
+                                    alt="YouTube Icon"
+                                    width={40}
+                                    height={40}
+                                    quality={100}
+                                />
+                            </a>
+                        </div>
+                        <div className={styles.eachIconCont}>
+                            <a target="_blank" rel="noreferrer noopener" href="https://open.spotify.com/artist/2DEWY12NlbFJk43W9Czhjb">
+                                <Image
+                                    src="/spotify.png"
+                                    alt="Spotify Icon"
+                                    width={40}
+                                    height={40}
+                                    quality={100}
+                                />
+                            </a>
+                        </div>
+                        <div className={styles.eachIconCont}>
+                            <a target="_blank" rel="noreferrer noopener" href="https://itunes.apple.com/us/artist/1484453514" >
+                                <Image
+                                    src='/imusic.png'
+                                    alt="Apple Music Icon"
+                                    width={40}
+                                    height={40}
+                                    quality={100}
+                                />
+                            </a>
+                        </div>
                     </div>
                     <div className={styles.songCover}> 
                         <Image
@@ -165,7 +222,28 @@ export default function Album(){
         )
     }
 
+    const creditDisplayer=(listOne, ListTwo, title)=>{
+        let musicians=listOne.map((elem, i)=>
+            <>
+            <div className={styles.musiciansList}> {elem} </div>
+            </>
+        )
 
+        let instruments=ListTwo.map((elem,i)=>
+        <>
+            <div className={styles.musiciansRole}> {elem} </div>
+        </>
+        )
+        return(
+            <>
+                <h2 className={styles.Artiststitle}> {title}</h2>
+                <div className={styles.MusiciansCreditBox}>
+                    <div> { musicians } </div>
+                    <div> { instruments } </div>
+                 </div>
+            </>
+        )
+    }
 
 
 
@@ -175,10 +253,30 @@ export default function Album(){
         <>
             <Header/>
             <div className={styles.albumPageGenCont}>
-            {albumStep==="main"&&
-                <> {albumMain()} </>} 
-            {albumStep==="song"&&
-                <> {songPlayer(pickedSong)} </>} 
+                {albumStep==="main"&&
+                    <> {albumMain()} </>} 
+                {albumStep==="song"&&
+                    <> {songPlayer(pickedSong)} </>}
+            </div>
+            <div style={{display:"flex", flexDirection:"row", justifyContent:"space-around"}}>
+                <div style={{display:"flex", flexDirection: "column"}}>
+                    <div className={styles.credits}>
+                        {creditDisplayer(musicCredits1, musicCredits2, "Artistas")}
+                    </div>
+                    <div className={styles.credits}>
+                        {creditDisplayer(musicCredits3,musicCredits4, "Equipo Tecnico")}
+                    </div>
+                </div>
+                <div >
+                    <div className={styles.BugOne}>
+                        <Image
+                            src="/bugOne.png"
+                            alt="Bug"
+                            width={700}
+                            height={400}
+                            />
+                    </div>        
+                </div>
             </div>
             <Footer/>
         </>
